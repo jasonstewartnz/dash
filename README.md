a6a515d3ae5caf046c2bf9e9b1739c89891df2e5
Jason Stewart <jason.stewart@gmail.com>
2023-01-29 13:23:26 -0800

    Dropdown directs level to present geo-data

---
+++ README.md
@@ -0,0 +1,25 @@
+# Create interactive visualizatin from Snowflake data
+
+
+# Features 
+
+Title
+Commentary text box
+
+Panel 1: Housing
+* Index-level selector (default=State)
+* Date Selector (default=latest)
+* Order by selector
+* Table with data 
+
+
+Panel 2: State Time series 
+* Select GEO_NAME multi-select
+* Time-series Value x Time
+
+
+
+
+Order
+1. Index-level selector
+2. Table with data 
--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from dash import Dash, html, dcc, dash_table
+from dash import Dash, html, dcc, dash_table, Output, Input
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
@@ -9,33 +9,57 @@ import pandas as pd
 from snowflake import connector
 from os import getenv
 
-# Gets the version
-ctx = connector.connect(
-    user=getenv('SNOWSQL_USER'),
-    password=getenv('SNOWSQL_PASSWORD'),
-    account=getenv('SNOWSQL_ACCOUNT'),
-    warehouse='COMPUTE_WH',
-    database='FHFA_SINGLE_FAMILY_HOME_APPRAISALS__VALUES',
-    schema='cybersyn'
-    )
 
-sql = """
-SELECT ind.level, geo_name, date, value 
-FROM cybersyn.fhfa_uad_timeseries ts
-JOIN cybersyn.fhfa_uad_attributes att ON ts.variable = att.variable
-JOIN cybersyn.fhfa_geo_index ind ON ind.id = ts.geo_id
-WHERE purpose = 'Purchase & Refinance'
-    AND characteristic = ''
-    AND ts.variable = 'Quarterly_Median_Appraised_Value__Purchase_&_Refinance_All_Appraisals'
-ORDER BY date
-"""
 
-housing_data = pd.read_sql( sql, ctx )
+def import_housing_data():
+    # Gets the version
+    ctx = connector.connect(
+        user=getenv('SNOWSQL_USER'),
+        password=getenv('SNOWSQL_PASSWORD'),
+        account=getenv('SNOWSQL_ACCOUNT'),
+        warehouse='COMPUTE_WH',
+        database='FHFA_SINGLE_FAMILY_HOME_APPRAISALS__VALUES',
+        schema='cybersyn'
+        )
 
+    sql = """
+    SELECT ind.level, geo_name, date, value 
+    FROM cybersyn.fhfa_uad_timeseries ts
+    JOIN cybersyn.fhfa_uad_attributes att ON ts.variable = att.variable
+    JOIN cybersyn.fhfa_geo_index ind ON ind.id = ts.geo_id
+    WHERE purpose = 'Purchase & Refinance'
+        AND characteristic = ''
+        AND ts.variable = 'Quarterly_Median_Appraised_Value__Purchase_&_Refinance_All_Appraisals'
+    ORDER BY date
+    """
+
+    housing_data = pd.read_sql( sql, ctx )
+
+    return housing_data
+
+def gen_geo_table():
+    data=housing_data.to_dict('records'),
+    columns=[{'name':col,'id':col} for col in display_cols],
+    style_cell=dict(textAlign='left'),
+    style_header=dict(backgroundColor="paleturquoise"),
+    style_data=dict(backgroundColor="lavender")       
+
+## Initialize
 app = Dash(__name__)
 
+housing_data = import_housing_data()
 levels = housing_data['LEVEL'].unique()
 display_cols = ['GEO_NAME','DATE','VALUE']
+
+
+@app.callback(
+    Output("housing-data-table", "data"), 
+    Input("geo-level-dropdown", "value"))
+def display_geo_for_level(level):     
+    
+    data=housing_data.loc[housing_data['LEVEL']==level,display_cols].to_dict('records')
+   
+    return data
 
 
 app.layout = html.Div(children=[
@@ -46,22 +70,19 @@ app.layout = html.Div(children=[
     '''),
 
     html.Div([
-        dcc.Dropdown(levels, 'Select Level', id='level-dropdown'),
+        dcc.Dropdown(levels, 'Select Level', id='geo-level-dropdown'),
         html.Div(id='dd-output-container')
     ]),
-    
 
     dash_table.DataTable(
-        id='housing-data-table',
-        data=housing_data.to_dict('records'),
+        id='housing-data-table', 
         columns=[{'name':col,'id':col} for col in display_cols],
         style_cell=dict(textAlign='left'),
         style_header=dict(backgroundColor="paleturquoise"),
-        style_data=dict(backgroundColor="lavender")        
+        style_data=dict(backgroundColor="lavender")            
     )
 
 ])
-
 
 
 # .loc[housing_data['LEVEL']==levels,display_cols]
